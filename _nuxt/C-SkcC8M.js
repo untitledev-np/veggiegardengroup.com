1a1cd49a9fcc22f8371b0685b157fdc07c969c21
chakulama <chaku.lama@gmail.com>
2024-05-17 01:32:04 +0200

    initial page update

---
+++ _nuxt/C-SkcC8M.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DlAUqK2U.js";import{c,o as n}from"./WcQcSfuM.js";const o={};function r(t,a){return n(),c("div",null," menu page ")}const f=e(o,[["render",r]]);export{f as default};
---
+++ _nuxt/D-ztLYpQ.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DmPCUwN6.js";import{_ as o}from"./DlAUqK2U.js";import{f as n,w as _,o as c,d as r}from"./WcQcSfuM.js";const a={};function s(f,m){const t=e;return c(),n(t,null,{default:_(()=>[r(" Sites page ")]),_:1})}const x=o(a,[["render",s]]);export{x as default};
