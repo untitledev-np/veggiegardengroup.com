bb67023616d03148fba3118f075d7ac0ff5a3c63
chakulama <chaku.lama@gmail.com>
2024-05-17 01:53:04 +0200

    udpate

---
+++ _nuxt/DkoD-Cxy.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DlAUqK2U.js";import{c as o,o as c}from"./ZOc5sGPx.js";const r={};function t(n,a){return c(),o("div",null," about page ")}const f=e(r,[["render",t]]);export{f as default};
