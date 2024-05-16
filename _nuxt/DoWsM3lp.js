82393406fae2b3d36093ca78912e885606ec375c
chakulama <chaku.lama@gmail.com>
2024-05-17 01:46:19 +0200

    udpate

--- _nuxt/DoWsM3lp.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{c,o as n}from"./D97V36GK.js";const o={};function r(t,a){return n(),c("div",null," menu page ")}const f=e(o,[["render",r]]);export{f as default};
