b6dc83916f30a91d4e8f4279f8705af46f0b0ffd
untitledev-np <sabingurung68@gmail.com>
2024-06-03 18:40:12 +0000

    PIPELINE - deploying build static web site

--- assets/BF9SjbLG.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./CB-UtIAA.js";import{_ as o}from"./DlAUqK2U.js";import{l as c,w as n,o as a,a as _}from"./D_9qD6YR.js";const s={},r=_("h2",{class:"text-3xl text-black font-bold mb-4 text-center"}," About page",-1);function l(x,f){const t=e;return a(),c(t,null,{default:n(()=>[r]),_:1})}const d=o(s,[["render",l]]);export{d as default};
--- assets/P8cY3wPN.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./CB-UtIAA.js";import{_ as o}from"./DlAUqK2U.js";import{l as n,w as c,o as a,a as _}from"./D_9qD6YR.js";const s={},r=_("h2",{class:"text-3xl text-black font-bold mb-4 text-center"},"Menu page",-1);function l(x,f){const t=e;return a(),n(t,null,{default:c(()=>[r]),_:1})}const d=o(s,[["render",l]]);export{d as default};
